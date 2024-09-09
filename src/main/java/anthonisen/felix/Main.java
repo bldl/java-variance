5f0606f3add641dc6505057d78dd5a7ef7c7b195
FelixAnthonisen <felix.anthonisen@gmail.com>
2024-09-09 19:16:44 +0200

    ast manipulator

--- src/main/java/anthonisen/felix/Main.java
+++ src/main/java/anthonisen/felix/Main.java
@@ -3,5 +3,7 @@ package anthonisen.felix;
 class Main {
     public static void main(String[] args) {
         System.out.println("Test");
+        AstManipulator manip = new AstManipulator();
+        manip.manipulateB();
     }
 }
