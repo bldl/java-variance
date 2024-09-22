2af515a44adaea42d6b8c0781fc8b55113a9022e
FelixAnthonisen <felix.anthonisen@gmail.com>
2024-09-23 00:54:03 +0200

    update example to contain more usecases

--- example/Main.java
+++ example/Main.java
@@ -7,9 +7,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Herd<Animal> animalHerd = new Herd<>(Arrays.asList(new Animal("Mickey"), new Animal("Poseidon")));
-        Herd<Animal> catHerd = new Herd<Animal>(Arrays.asList(new Cat("Katty"), new Cat("Mons")));
-        catHerd.get(0).feed();
-        Collection<Animal> allCats = catHerd.getAll();
+        Herd<Cat> catHerd = new Herd<>(Arrays.asList(new Cat("Katty"), new Cat("Mons")));
+
+        Cat katty = catHerd.get(0);
+
+        Collection<Cat> allCats = catHerd.getAll();
+
+        for (Cat cat : allCats) {
+            cat.feed();
+        }
+
         Herd.feedAll(animalHerd);
     }
 }
