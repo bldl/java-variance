076a4bf1149f7554fda8c989ef6272d7edd893a3
FelixAnthonisen <felix.anthonisen@gmail.com>
2024-09-23 00:56:08 +0200

    implement TypeReplacer

---
+++ src/main/java/anthonisen/felix/astParsing/util/TypeReplacer.java
@@ -0,0 +1,26 @@
+package anthonisen.felix.astParsing.util;
+
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
+
+public class TypeReplacer {
+    public static boolean replaceTypes(Type type, String targetTypeName, String newTypeName) {
+        boolean found = false;
+        if (type instanceof ClassOrInterfaceType) {
+            ClassOrInterfaceType classType = (ClassOrInterfaceType) type;
+            System.out.println(type);
+            if (classType.asString().equals(targetTypeName)) {
+                found = true;
+                classType.replace(new ClassOrInterfaceType(null, newTypeName));
+                classType.setName(newTypeName);
+            }
+
+            if (classType.getTypeArguments().isPresent()) {
+                for (Type arg : classType.getTypeArguments().get()) {
+                    found = replaceTypes(arg, targetTypeName, newTypeName) || found;
+                }
+            }
+        }
+        return found;
+    }
+}
