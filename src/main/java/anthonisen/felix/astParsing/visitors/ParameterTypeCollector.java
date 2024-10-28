9ef7d017d1a68b9d15f98cf595ab267a39c8ca24
FelixAnthonisen <felix.anthonisen@gmail.com>
2024-10-28 21:50:44 +0100

    change visitor to not collect constructor types

--- src/main/java/anthonisen/felix/astParsing/visitors/ParameterTypeCollector.java
+++ src/main/java/anthonisen/felix/astParsing/visitors/ParameterTypeCollector.java
@@ -2,14 +2,19 @@ package anthonisen.felix.astParsing.visitors;
 
 import java.util.Set;
 
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 public class ParameterTypeCollector extends VoidVisitorAdapter<Set<Type>> {
+
+  @SuppressWarnings("unchecked")
   @Override
   public void visit(Parameter n, Set<Type> arg) {
     super.visit(n, arg);
-    arg.add(n.getType());
+
+    if (!n.findAncestor(ConstructorDeclaration.class).isPresent())
+      arg.add(n.getType());
   }
 }
